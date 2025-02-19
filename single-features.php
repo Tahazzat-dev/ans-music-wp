9cabe500f4ff69a0967df1c9355a260374db307a
Tahazzat-dev <mdtahazzatali820@gmail.com>
2025-02-19 19:24:20 +0600

    related features added

--- single-features.php
+++ single-features.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  *Template Name: Feature details page
  *
@@ -9,123 +9,110 @@ get_header();
 ?>
 
 <main class="ANS_main-wrapper">
-      <section class="ANS_about-banner-wrapper ANS_single-feature-wrap">
-        <!-- banner content -->
-        <div class="ANS_about-banner-bg">
-          <picture>
-            <source
-              media="(min-width: 768px)"
-              srcset="<?php echo get_field('feature_banner_image')?>"
-            />
-            <source
-              media="(max-width: 767px)"
-            srcset="<?php echo get_field('feature_banner_image')?>"
-            />
-            <img  data-aos="fade-down" src="<?php echo get_field('feature_banner_image');?>" alt="Banner image" />
-          </picture>
+  <section class="ANS_about-banner-wrapper ANS_single-feature-wrap">
+    <!-- banner content -->
+    <div class="ANS_about-banner-bg">
+      <picture>
+        <source media="(min-width: 768px)" srcset="<?php echo get_field('feature_banner_image') ?>" />
+        <source media="(max-width: 767px)" srcset="<?php echo get_field('feature_banner_image') ?>" />
+        <img data-aos="fade-down" src="<?php echo get_field('feature_banner_image'); ?>" alt="Banner image" />
+      </picture>
 
-          <div class="banner-overlay"></div>
-        </div>
+      <div class="banner-overlay"></div>
+    </div>
 
-        <!-- banner content -->
-        <div class="ANS_about-banner-container ANS_flex flex_column">
-          <div
-           style="max-width:1000px; overflow: hidden;"
-            class="ANS_about-banner-content ANS_container ANS_flex flex_column flex_align_center justify_center"
-          >
-           <h5 class="fs-lg"><?php the_title(); ?></h5>
-            <h2 class="fs-4xl text-center">
-              <?php echo get_field('feature_sub_title'); ?>
-            </h2>
-            <p><?php  echo get_field('feature_description') ?></p>
-          </div>
-        </div>
-      </section>
-      <?php
-      
-      $features_lists = get_field('feature_specification_lists');
-
-    if(!empty($features_lists)){
-        echo '<section class="ANS_service-wrapper ANS_flex flex_column">';
-        foreach($features_lists as $feature){
-            ?>
-            <!-- ANS_services -->
-    <div class="ANS_service-container ANS_container ANS_flex flex_align_center justify_between">
-      <div class="section-thum">
-        <img data-aos="fade-down" src="<?php echo $feature['thum_img']; ?>" alt="Music plartform image" />
-      </div>
-
-      <div class="content ANS_flex flex_column">
-        <h3 data-aos="fade-up" class="fs-3xl"><?php echo $feature['title']; ?></h3>
-        <p data-aos="fade-up">
-          <?php echo $feature['description']; ?>
-        </p>
-        <button data-aos="fade-up" class="ANS_btn btn-b-yellow">GET STARTED</button>
+    <!-- banner content -->
+    <div class="ANS_about-banner-container ANS_flex flex_column">
+      <div style="max-width:1000px; overflow: hidden;"
+        class="ANS_about-banner-content ANS_container ANS_flex flex_column flex_align_center justify_center">
+        <h5 class="fs-lg-lh-md"><?php the_title(); ?></h5>
+        <h2 class="fs-4xl text-center">
+          <?php echo get_field('feature_sub_title'); ?>
+        </h2>
+        <p><?php echo get_field('feature_description') ?></p>
       </div>
     </div>
-    <!-- ANS_services -->         
-    <?php 
+  </section>
+  <?php
+
+  $features_lists = get_field('feature_specification_lists');
+
+  if (!empty($features_lists)) {
+    echo '<section class="ANS_service-wrapper ANS_flex flex_column">';
+    foreach ($features_lists as $feature) {
+      ?>
+      <!-- ANS_services -->
+      <div class="ANS_service-container ANS_container ANS_flex flex_align_center justify_between">
+        <div class="section-thum">
+          <img data-aos="fade-down" src="<?php echo $feature['thum_img']; ?>" alt="Music plartform image" />
+        </div>
+
+        <div class="content ANS_flex flex_column">
+          <h3 data-aos="fade-up" class="fs-3xl"><?php echo $feature['title']; ?></h3>
+          <p data-aos="fade-up">
+            <?php echo $feature['description']; ?>
+          </p>
+          <button data-aos="fade-up" class="ANS_btn btn-b-yellow">GET STARTED</button>
+        </div>
+      </div>
+      <!-- ANS_services -->
+      <?php
     }
-echo '</section>';
-    }
-    ?>
+    echo '</section>';
+  }
+  ?>
 
 
 
   <!-- distribution partners -->
+  <?php
+  global $post;
+  $current_slug = $post->post_name;
+  $query_args = array(
+    'post_type' => 'features',
+    'posts_per_page' => 10,
+    'post_status' => 'publish',
+    'orderby' => 'date',
+    'order' => 'ASC'
+  );
+
+  $feature_query = new WP_Query($query_args);
+  ?>
   <div class="ANS_distribution-partners-wrap features-wrap">
-           <div class="ANS_distribution-partners-inner ANS_container">
-                <h2 class="fs-5xl text-fill neon-stroke text-center"  >
-                    <span data-text="EXPLORE">EXPLORE</span>
-                </h2>
+    <div class="ANS_distribution-partners-inner ANS_container">
+      <h2 class="fs-5xl text-fill neon-stroke text-center">
+        <span data-text="EXPLORE">EXPLORE</span>
+      </h2>
+      <div class="distribution-partners">
+        <?php
+        if ($feature_query->have_posts()):
+          while ($feature_query->have_posts()):
+            $feature_query->the_post();
+            $post_slug = get_post_field('post_name', get_the_ID()); 
+            if ($current_slug === $post_slug) {
+              continue;
+            }
+            ?>
 
-                <div class="distribution-partners">
-
-                <!-- card -->
-                <a href="#" class="card">
-                    <div class="glow"></div>
-                    <img src="<?php echo get_template_directory_uri(). '/assets/icons/analytics-and-insights.svg';?>" alt="partners icon">
-                    <h3 class="fs-md">Analytics & Insights</h3>
-                </a>
-                <!-- card -->
-                <a href="#" class="card">
-                    <div class="glow"></div>
-                    <img src="<?php echo get_template_directory_uri(). '/assets/icons/catalog-management.svg';?>" alt="partners icon">
-                    <h3 class="fs-md">Catalog Management</h3>
-                </a>
-                <!-- card -->
-                <a href="#" class="card">
-                    <div class="glow"></div>
-                    <img src="<?php echo get_template_directory_uri(). '/assets/icons/distribution-and-delivery.svg';?>" alt="partners icon">
-                    <h3 class="fs-md">Rights Management</h3>
-                </a>
-
-                <!-- card -->
-                <a href="#" class="card">
-                    <div class="glow"></div>
-                    <img src="<?php echo get_template_directory_uri(). '/assets/icons/distribution-and-delivery.svg';?>" alt="partners icon">
-                    <h3 class="fs-md">Distribution & Delivery</h3>
-                </a>
-                <!-- card -->
-                <a href="#" class="card">
-                    <div class="glow"></div>
-                    <img src="<?php echo get_template_directory_uri(). '/assets/icons/global-payments.svg';?>" alt="partners icon">
-                    <h3 class="fs-md">Royalty Accounting</h3>
-                </a>
-                <!-- card -->
-                <a href="#" class="card">
-                    <div class="glow"></div>
-                    <img src="<?php echo get_template_directory_uri(). '/assets/icons/income-tracking.svg';?>" alt="partners icon">
-                    <h3 class="fs-md">Income Tracking</h3>
-                </a>
-                <!-- card -->
-           </div>
-           </div>
-
-          
-       </div>
+            <!-- card -->
+            <a href="<?php the_permalink(); ?>" class="card">
+              <div class="glow"></div>
+              <img src="<?php echo get_template_directory_uri() . '/assets/icons/analytics-and-insights.svg'; ?>"
+                alt="partners icon">
+              <h3 class="fs-md"><?php the_title(); ?></h3>
+            </a>
+            <!-- card -->
+            <?php
+          endwhile;
+        endif;
+        ?>
+      </div>
+    </div>
 
 
-    </main>
+  </div>
+
+
+</main>
 <?php
 get_footer();
